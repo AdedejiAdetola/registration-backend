1547d6d538bbbda23342a8f9bc9bf2da89f9781c
Adetola Adedeji <adetolaadedeji0@gmail.com>
2023-06-16 04:11:56 +0100

    Cors

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.use(
     })
 );
 
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 app.use('/regRoutes', regRoutes)
 
